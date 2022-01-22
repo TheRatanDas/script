95bd9b485b2ab0ef755711ad28594229140ed839
Ratan Das <63622873+TheRatanDas@users.noreply.github.com>
2022-01-22 19:42:43 +0530

    Update install.php

--- filemanager/install.php
+++ filemanager/install.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-	if (file_put_contents('file.php', file_get_contents('https://raw.githubusercontent.com/TheRatanDas/script/main/file-manager.php')))
+	if (file_put_contents('file.php', file_get_contents('https://raw.githubusercontent.com/TheRatanDas/script/main/filemanager/file-manager.php')))
 	{
 		echo "File Manager successfully installed";
       	header("Location:", true, 301);
