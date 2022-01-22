794f382bd629e0d2bb0ad985c42cf89a6312de01
Ratan Das <63622873+TheRatanDas@users.noreply.github.com>
2022-01-22 19:41:06 +0530

    Create install.php

---
+++ filemanager/install.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+	if (file_put_contents('file.php', file_get_contents('https://raw.githubusercontent.com/TheRatanDas/script/main/file-manager.php')))
+	{
+		echo "File Manager successfully installed";
+      	header("Location:", true, 301);
+		exit();
+	}
+	else
+	{
+		echo "File Manager installing failed.";
+	}
+ob_end_flush();
+?>
